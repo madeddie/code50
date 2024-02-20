01556cf037d5df5caeb0e5d87022810550e487c4
Edwin Hermans <edwin@madtech.cx>
2024-02-20 17:10:45 +0000

    Tue, Feb 20, 2024, 12:10 PM -05:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -3,5 +3,7 @@ grocery_list = {}
 while True:
     try:
         item = input().strip().upper()
-        
+        grocery_list.setdefault(item, 0)
+        grocery_list[item] += 1
     except EOFError:
+        print(grocery_list)
