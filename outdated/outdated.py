de3f4fbc672764e942009040f6b6b6b7ef243b1f
Edwin Hermans <edwin@madtech.cx>
2024-02-20 17:25:37 +0000

    Tue, Feb 20, 2024, 12:25 PM -05:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -16,8 +16,8 @@ months = [
 input_date = input("Date: ")
 if '/' in input_date:
     month, day, year = input_date.split('/')
-    print(f'{year}-{month:02}-{day:02}')
+    print(f'{year}-{int(month):02}-{int(day):02}')
 elif input_date.split(" ")[0] in months:
     txt_month, day, year = input_date.split(" ")
     month = months.index(txt_month) + 1
-    print(f'{year}-{month:02}-{day:02}')
+    print(f'{year}-{month:02}-{int(day):02}')
