aba217be2a70b87bbca217822d812c4fac83d1ee
Edwin Hermans <edwin@madtech.cx>
2024-02-20 04:53:25 +0000

    Mon, Feb 19, 2024, 11:53 PM -05:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,8 +1,9 @@
 camel_var = input("Gimme the humps: ")
 
->>> output = ""
->>> for char in name:
-...   if char.isupper():
-...     output += "_" + char.lower()
-...   else:
-...     output += char
+output = ""
+for char in name:
+  if char.isupper():
+    output += "_" + char.lower()
+  else:
+    output += char
+print(output)
