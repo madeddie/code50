4aae8a07a8723e7a941c1ac462de7ac7546eef57
Edwin Hermans <edwin@madtech.cx>
2024-02-20 05:22:47 +0000

    Tue, Feb 20, 2024, 12:22 AM -05:00

--- plates/plates.py
+++ plates/plates.py
@@ -27,7 +27,9 @@ def starts_with_2_letters(s):
 # “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
 def min_2_max_6(s):
     return 6 >= s >= 2
-“Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
+
+# “Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
+
 “No periods, spaces, or punctuation marks are allowed.”
 
 
