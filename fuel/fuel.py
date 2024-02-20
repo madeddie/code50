458649d3b0562f814941ab1fbeed6d3ab4408dba
Edwin Hermans <edwin@madtech.cx>
2024-02-20 16:36:18 +0000

    Tue, Feb 20, 2024, 11:36 AM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -5,6 +5,8 @@ while True:
     percentage = round(int(x) / int(y) * 100)
     if percentage <= 1:
       print("E")
+    elif percentage > 100:
+      continue
     elif percentage >= 99:
       print("F")
     else:
