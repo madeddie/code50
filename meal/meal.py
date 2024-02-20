cc18c7db9b52b7f438aa194fbff53a9bb6b9e8bc
Edwin Hermans <edwin@madtech.cx>
2024-02-20 03:53:12 +0000

    Mon, Feb 19, 2024, 10:53 PM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,17 +1,17 @@
 def main():
     mealtime = convert(input("So you hungry now? ").strip())
-    if 8 >= mealtime > 7:
+    if 8 >= mealtime >= 7:
         print("breakfast time")
-    elif 12 >= mealtime > 13:
+    elif 12 >= mealtime >= 13:
         print("lunch time")
-    elif 18 >= mealtime > 19:
+    elif 18 >= mealtime >= 19:
         print("dinner time")
 
 
 def convert(time):
     hours, minutes = time.split(":")
-    minute_fract = minutes / 60
-    return hours + minute_fract
+    minute_fract = int(minutes) / 60
+    return int(hours) + minute_fract
 
 
 if __name__ == "__main__":
