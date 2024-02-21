c5ce58de4805aa7af5a5f47328694d7143f57f11
Edwin Hermans <edwin@madtech.cx>
2024-02-21 20:26:58 +0000

    Wed, Feb 21, 2024, 3:26 PM -05:00

--- professor/professor.py
+++ professor/professor.py
@@ -3,6 +3,21 @@ import sys
 
 def main():
     level = get_level()
+    problems = []
+
+    for i in range(10):
+        problems.append((generate_integer(level), generate_integer(level)))
+
+    for problem in problems:
+        tries = 3
+        while tries > 0:
+            answer = input(f"{problem[0]} + {problem[1]} = ")
+            if int(answer) == problem[0] + problem[1]:
+                break
+            else:
+                print("EEE")
+                tries -= 1
+
 
 def get_level():
     while True:
@@ -20,16 +35,6 @@ def get_level():
 def generate_integer(level):
     if level not in [1, 2, 3]:
         raise ValueError
-    
-problems = []
+    return random.randrange(10 ** (level - 1), 10 ** level)
 
-for i in range(10):
-    problems.append((random.randrange(10 ** (level - 1), 10 ** level), random.randrange(10 ** (level - 1), 10 ** level)))
-
-for problem in problems:
-    tries = 3
-    while True:
-        answer = input(f"{problem[0]} + {problem[1]}")
-        if answer == problem[0] + problem[1]:
-            continue
-        else:
+main()
