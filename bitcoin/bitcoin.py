b3c4b6496d1419f98f4cde119cdaeec0ea4532ec
Edwin Hermans <edwin@madtech.cx>
2024-02-21 21:16:06 +0000

    Wed, Feb 21, 2024, 4:16 PM -05:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -3,11 +3,15 @@ import sys
 import requests
 
 try:
-    amount = float(sys.argv[1])
+    input_amount = float(sys.argv[1])
 except:
     sys.exit("Not a valid amount")
 
 try:
-    ...
+    usd_rate = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json').json().get('bpi').get('USD').get('rate_float')
 except requests.RequestException:
-    ...
+    sys.exit("Oops, something went wrong")
+
+amount = input_amount * usd_rate
+
+print(f"${amount:,.4f}")
