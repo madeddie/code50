a301e4d3d7eef17a32a6df8395fa03515aa4049e
Edwin Hermans <edwin@madtech.cx>
2024-02-21 19:13:10 +0000

    Wed, Feb 21, 2024, 2:13 PM -05:00

--- game/game.py
+++ game/game.py
@@ -10,3 +10,17 @@ while True:
         continue
 
 random_val = random.randrange(1, level)
+
+def get_guess():
+    while True:
+        try:
+            guess = int(input("Guess: "))
+            if guess < 1:
+                continue
+            break
+        except:
+            continue
+
+    return guess
+
+
