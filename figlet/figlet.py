42e9fe75a65d34047508a58909a736b146b93a78
Edwin Hermans <edwin@madtech.cx>
2024-02-21 18:38:20 +0000

    Wed, Feb 21, 2024, 1:38 PM -05:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,13 +6,14 @@ from pyfiglet import Figlet
 figlet = Figlet()
 
 if len(sys.argv) == 1:
-    font = random.choices(figlet.getFonts())[0]
+    figlet.setFont(font=random.choices(figlet.getFonts())[0])
 elif len(sys.argv) == 3:
     if sys.argv[1] not in ['-f', '--font']:
         sys.exit('Invalid option')
     else:
         if not sys.argv[2] in figlet.getFonts():
             sys.exit('Invalid font')
-    font = sys.argv[2]
+    figlet.setFont(font=sys.argv[2])
 
 output_text = input('Input: ')
+print(figlet.renderText(output_text))
