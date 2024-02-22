025fab6af279b61a687202367746d58cd8971836
Edwin Hermans <edwin@madtech.cx>
2024-02-22 18:32:15 +0000

    Thu, Feb 22, 2024, 1:32 PM -05:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -14,14 +14,16 @@ if len(sys.argv) != 3:
     sys.exit(f"Usage: {sys.argv[0]} <INPUT> <OUTPUT>")
 elif check_ext(sys.argv[1]) != check_ext(sys.argv[2]):
     sys.exit(f"Files need to have the same extension")
-else:
-    infile = sys.argv[1]
-    outfile = sys.argv[2]
 
 try:
     shirt = Image.open("shirt.png")
 except "FileNotFoundError":
-    sys.exit("File does not exist")
+    sys.exit("shirt.png does not exist")
 
+try:
+    infile = Image.open(sys.argv[1])
+except "FileNotFoundError":
+    sys.exit(f"{infile} does not exist")
 #output = photo.paste(shirt, shirt)
-shirt.save('somefile.png')
+
+print(infile.size)
