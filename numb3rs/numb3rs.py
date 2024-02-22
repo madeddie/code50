4e61a9037e9b9a78b2d0cb46eb477de9823e8922
Edwin Hermans <edwin@madtech.cx>
2024-02-22 19:06:00 +0000

    Thu, Feb 22, 2024, 2:06 PM -05:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -9,10 +9,10 @@ def main():
 def validate(ip):
     parts = ip.split(".")
     for part in parts:
-        if 0 > part > 255
-
-
-...
+        if 0 > int(part) > 255:
+            return False
+        else:
+            return True
 
 
 if __name__ == "__main__":
