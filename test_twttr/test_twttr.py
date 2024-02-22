0a56001bad1d66e60dc01704b2db06e8e3a6281e
Edwin Hermans <edwin@madtech.cx>
2024-02-22 14:30:05 +0000

    Thu, Feb 22, 2024, 9:30 AM -05:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,1 +1,10 @@
 from twttr import shorten
+
+def test_lowercase():
+    assert shorten("this is a test") == "ths s  tst"
+
+def test_uppercase():
+    assert shorten("THIS IS A TEST") == "THS S  TST"
+
+def test_mixedcase():
+    assert shorten("This Is A Test") == "Ths s  Tst"
