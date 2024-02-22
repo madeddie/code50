5c96fc1422048ede2a79f770bdd29d69976b050f
Edwin Hermans <edwin@madtech.cx>
2024-02-22 14:49:29 +0000

    Thu, Feb 22, 2024, 9:49 AM -05:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,3 +1,15 @@
 from plates import is_valid
 
-def test_
+def test_start_with_2_letters():
+    assert is_valid("AB123") == True
+    assert is_valid("A123") == False
+
+def test_max6_min2():
+    assert is_valid("AB1234") == True
+    assert is_valid("AB12345") == False
+    assert is_valid("A") == False
+
+def test_numbers_at_end():
+    assert is_valid("AB123") == True
+    assert is_valid("A123B") == False
+    assert is_valid("123AB") == False
