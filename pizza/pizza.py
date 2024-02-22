c3309f26e3b0b5d94bb062d915a59029df527634
Edwin Hermans <edwin@madtech.cx>
2024-02-22 17:37:53 +0000

    Thu, Feb 22, 2024, 12:37 PM -05:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,6 +1,9 @@
 import csv
 import sys
 
+from tabulate import tabulate
+
+
 if len(sys.argv) != 2:
     sys.exit("Please give only 1 command-line arguments")
 elif not sys.argv[1].endswith(".csv"):
@@ -11,7 +14,7 @@ else:
     try:
         with open(filename, "r") as csvfile:
             reader = csv.DictReader(csvfile)
-            for row in reader:
-                print(row)
     except "FileNotFoundError":
         sys.exit("File does not exist")
+
+print(tabulate(reader))
