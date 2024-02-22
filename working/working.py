15a947c973f2d450227b892b3abe9b1e24bd0944
Edwin Hermans <edwin@madtech.cx>
2024-02-22 19:48:57 +0000

    Thu, Feb 22, 2024, 2:48 PM -05:00

--- working/working.py
+++ working/working.py
@@ -9,14 +9,17 @@ def parse_time(s):
     time, meridiem = s.split(" ")
     if ":" in time:
         hour, minute = time.split()
-        if 1 > int(hour) > 12 or 0 > int(minute) > 60:
-            raise ValueError
+    else:
+        hour = time
+        minute = 0
+
+    if 1 > int(hour) > 12 or 0 > int(minute) > 60:
+        raise ValueError
 
 
 def convert(s):
     start, stop = re.search("(.*? (?:AM|PM)) to (.*? (?:AM|PM))", s).groups()
-    if ':' in start:
-        hour, minute = start.split()
+    parse_time(start)
 
 
 if __name__ == "__main__":
