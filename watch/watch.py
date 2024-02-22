e597821a2946eda0ba4416204ff2ac43aa323b22
Edwin Hermans <edwin@madtech.cx>
2024-02-22 19:30:41 +0000

    Thu, Feb 22, 2024, 2:30 PM -05:00

--- watch/watch.py
+++ watch/watch.py
@@ -7,10 +7,8 @@ def main():
 
 
 def parse(s):
-    ...
-
-
-...
+    res = re.search("src=\"https://.*?\.youtube\.com/embed/(.*?)\"", s)
+    return(f"https://youtu.be/{res[1]}")
 
 
 if __name__ == "__main__":
