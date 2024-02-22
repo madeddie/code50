3e6aa6b705327dc93130aa71e683bacecdafcd60
Edwin Hermans <edwin@madtech.cx>
2024-02-22 15:22:42 +0000

    Thu, Feb 22, 2024, 10:22 AM -05:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -2,15 +2,11 @@ import pytest
 
 from fuel import convert, gauge
 
-""" def test_convert():
-    assert convert("2/3") == 67
-    with pytest.raises(ValueError):
-        assert convert("cat/dog")
-    with pytest.raises(ValueError):
-        assert convert("3/2")
-    with pytest.raises(ZeroDivisionError):
-        assert convert("0/0") """
 
+def test_convert_success():
+    assert convert("2/3") == 67
+    assert convert("1/2") == 50
+    assert convert("3/4") == 75
 
 def test_convert_fail_int():
     with pytest.raises(ValueError):
