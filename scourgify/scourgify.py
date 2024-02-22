5390a38090eb4ce1423083de1d5d699e4f46d5c9
Edwin Hermans <edwin@madtech.cx>
2024-02-22 17:56:26 +0000

    Thu, Feb 22, 2024, 12:56 PM -05:00

---
+++ scourgify/scourgify.py
@@ -0,0 +1,17 @@
+import csv
+import sys
+
+if len(sys.argv) != 3:
+    sys.exit(f"Usage: {sys.argv[0]} <INFILE> <OUTFILE>")
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+
+try:
+    with open(infile, "r") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            firstname, lastname = row.split()
+            print(row)
+except "FileNotFoundError":
+    sys.exit("File does not exist")
