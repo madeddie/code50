994f1995061ad4a260ad3222b524c66b811bc970
Edwin Hermans <edwin@madtech.cx>
2024-02-22 18:16:48 +0000

    Thu, Feb 22, 2024, 1:16 PM -05:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -2,12 +2,19 @@ import csv
 import sys
 
 
+def check_ext(filename):
+    extension = filename.split(".")[-1]
+    if extension.lower() not in ['jpg', 'jpeg', 'png']:
+        sys.exit("Wrong file format, only allow jpg and png")
+
+    return extension.lower()
+
 if len(sys.argv) != 3:
-    sys.exit(f"Usage: {sys.argv[0] }")
-elif not sys.argv[1].endswith(".csv"):
-    sys.exit("Not a CSV file")
+    sys.exit(f"Usage: {sys.argv[0] <INPUT> <OUTPUT>}")
+elif check_ext(sys.argv[1]) != check_ext(sys.argv[2]):
+    sys.exit(f"Files need to have the same extension")
 else:
-    filename = sys.argv[1]
+    infile = sys.argv[1]
     lines = []
     try:
         with open(filename, "r") as csvfile:
