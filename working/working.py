e7d60341daf43e1ec80d5ddce19e9c08b71d9ac0
Edwin Hermans <edwin@madtech.cx>
2024-02-22 20:12:10 +0000

    Thu, Feb 22, 2024, 3:12 PM -05:00

--- working/working.py
+++ working/working.py
@@ -19,7 +19,7 @@ def parse_time(s):
     return(int(hour), int(minute), meridiem)
 
 def generate_24hr(hour, minute, meridiem):
-    if meridiem == "PM" and hour > 12:
+    if meridiem == "PM" and hour < 12:
         hour = hour + 12
     return(f"{hour:02}:{minute:02}")
 
