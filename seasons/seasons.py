4909bd2b457c375b20548d4119fd1a1b9a8889c8
Edwin Hermans <edwin@madtech.cx>
2024-02-23 18:40:19 +0000

    Fri, Feb 23, 2024, 1:40 PM -05:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,16 +1,23 @@
 from datetime import date
 
+import inflect
+
+p = inflect.engine()
+
 
 # Date of Birth: 1999-12-31
 # One thousand, four hundred forty minutes
 def main():
     birthdate = input("Date of Birth: ")
     minutes = get_minutes(birthdate)
-    print(minutes)
+    print(f"{inflect_time(minutes).capitalize()} minutes")
 
 def get_minutes(bd):
     year, month, day = bd.split("-")
     return (date.today() - date(int(year), int(month), int(day))).days * 24 * 60
+
+def inflect_time(minutes):
+    return p.number_to_words(minutes)
 
 if __name__ == "__main__":
     main()
