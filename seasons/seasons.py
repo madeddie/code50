a09fc37797b17841ab586c70d703384dcf28def0
Edwin Hermans <edwin@madtech.cx>
2024-02-23 18:27:52 +0000

    Fri, Feb 23, 2024, 1:27 PM -05:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,12 +1,13 @@
 from datetime import date
 
 
+# Date of Birth: 1999-12-31
+# One thousand, four hundred forty minutes
 def main():
-    ...
+    birthdate = input("Date of Birth: ")
 
 
-...
-
-
+def minutes(date):
+    today = date.today()
 if __name__ == "__main__":
     main()
