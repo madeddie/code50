42ddbb1ea5597e1726dfe0110b4c9fb40c5320ba
Edwin Hermans <edwin@madtech.cx>
2024-02-23 18:36:03 +0000

    Fri, Feb 23, 2024, 1:36 PM -05:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -5,10 +5,11 @@ from datetime import date
 # One thousand, four hundred forty minutes
 def main():
     birthdate = input("Date of Birth: ")
+    minutes = get_minutes(birthdate)
+    print(minutes)
 
-
-def minutes(date):
-    year, month, day = date.split("-")
+def get_minutes(bd):
+    year, month, day = bd.split("-")
     return (date.today() - date(int(year), int(month), int(day))).days * 24 * 60
 
 if __name__ == "__main__":
