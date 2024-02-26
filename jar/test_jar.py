004ef246d2ff4b4c666b15eab7577d3cb143fcba
Edwin Hermans <edwin@madtech.cx>
2024-02-26 20:37:09 +0000

    Mon, Feb 26, 2024, 3:37 PM -05:00

---
+++ jar/test_jar.py
@@ -0,0 +1,36 @@
+import pytest
+
+from jar import Jar
+
+
+def test_init():
+    jar = Jar()
+    assert jar.capacity == 12
+    jar = Jar(capacity=15)
+    assert jar.capacity == 15
+    with pytest.raises(ValueError):
+        jar = Jar(capacity=-1)
+
+def test_str():
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(11)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+
+
+def test_deposit():
+    jar = Jar(capacity=12)
+    jar.deposit(3)
+    assert jar.size == 3
+    with pytest.raises(ValueError):
+        jar.deposit(10)
+
+
+def test_withdraw():
+    jar = Jar(capacity=5)
+    jar.withdraw(2)
+    assert jar.capacity == 3
+    with pytest.raises(ValueError):
+        jar.withdraw(4)
