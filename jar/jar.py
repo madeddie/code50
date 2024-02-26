09cddacb810b2d27736f8a13b2fab5acccec1a75
Edwin Hermans <edwin@madtech.cx>
2024-02-26 20:22:44 +0000

    Mon, Feb 26, 2024, 3:22 PM -05:00

---
+++ jar/jar.py
@@ -0,0 +1,38 @@
+class Jar:
+    def __init__(self, capacity=12):
+        if not capacity.isdecimal():
+            raise ValueError
+        elif int(capacity) < 0:
+            raise ValueError
+
+        self.capacity = capacity
+        self.size = 0
+
+    def __str__(self):
+        return "🍪" * self.capacity
+
+    def deposit(self, n):
+        if (self.size + n) > self.capacity:
+            raise ValueError
+
+        self.size += n
+
+    def withdraw(self, n):
+        if (self.size - n) < 0:
+            raise ValueError
+
+        self.size -= n
+
+    @property
+    def capacity(self):
+        return self.capacity
+
+    @property
+    def size(self):
+        return self.size
+
+def main():
+    jar = Jar(capacity=15)
+
+if __name__ == "__main__":
+    main()
