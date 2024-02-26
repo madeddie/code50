5e063ed2a847846d8be6b6d073ac29c927cff3ed
Edwin Hermans <edwin@madtech.cx>
2024-02-26 20:58:41 +0000

    Mon, Feb 26, 2024, 3:58 PM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -5,7 +5,7 @@ def main():
     pdf.add_page()
     pdf.set_font("Times", size=12)
     pdf.cell(w=0,h=0,text="CS50 Shirtificate",align='C')
-    pdf.image("shirtificate.png", x=20, y=20)
+    pdf.image("shirtificate.png", x=0, y=20)
     pdf.output("shirtificate.pdf")
 
 if __name__ == "__main__":
