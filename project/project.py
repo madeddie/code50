8a96468636bdad901fd43fd5b5296a0a0fe31a78
Edwin Hermans <edwin@madtech.cx>
2024-02-28 15:13:16 +0000

    Wed, Feb 28, 2024, 10:13 AM -05:00

--- project/project.py
+++ project/project.py
@@ -2,9 +2,9 @@
 # This is a game of yahtzee on the CLI
 # made by edwin@madtech.cx for CS50 Python
 
-DIE_FACE_1: ⚀
-DIE_FACE_2: ⚀
-DIE_FACE_3: ⚀
-DIE_FACE_4: ⚀
-DIE_FACE_5: ⚀
-DIE_FACE_6: ⚀
+DIE_FACE_1: "\u2680'
+DIE_FACE_2: "\u2681'
+DIE_FACE_3: "\u2682'
+DIE_FACE_4: "\u2683'
+DIE_FACE_5: "\u2684'
+DIE_FACE_6: "\u2685'
