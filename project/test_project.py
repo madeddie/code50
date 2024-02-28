a769b958446b5275a8bbc5521e6c5fa1cf84e3a5
Edwin Hermans <edwin@madtech.cx>
2024-02-28 18:17:15 +0000

    Wed, Feb 28, 2024, 1:17 PM -05:00

--- project/test_project.py
+++ project/test_project.py
@@ -7,4 +7,7 @@ def test_roll_dice():
     assert project.roll_dice(5) == [4, 4, 1, 3, 5]
     assert project.roll_dice(6) == [4, 4, 3, 4, 3, 5]
 
-def
+def test_visualize_dice():
+    hand = [1, 2, 3, 4, 5]
+    assert project.visualize_dice(hand, style="unicode") == "⚀ ⚁ ⚂ ⚃ ⚄"
+    assert project.visualize_dice(hand) == ""
