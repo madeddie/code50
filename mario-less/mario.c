f1ce0520f8644f4864602b7466a385afe83890bd
Edwin Hermans <edwin@madtech.cx>
2024-02-29 01:55:19 +0000

    Wed, Feb 28, 2024, 8:55 PM -05:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -17,11 +17,11 @@ int main(void)
     for (int i = 0; i < n; i++)
     {
         // Print row of bricks
-        print_row(i + 1);
+        print_row(i + 1, n);
     }
 }
 
-void print_row(int bricks)
+void print_row(int bricks, int height)
 {
     for (int i = 0; i < bricks; i++)
     {
