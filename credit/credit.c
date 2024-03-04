7d8b877401b5e6052cb87c53ecd38c49424d7fba
Edwin Hermans <edwin@madtech.cx>
2024-03-04 02:50:48 +0000

    Sun, Mar 3, 2024, 9:50 PM -05:00

---
+++ credit/credit.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    long ccnum = input_long("Number: ")
+    int counteven = 0;
+    int countodd = 0;
+    int i = 1;
+
+    while (ccnum > 0)
+    {
+        if (i % 2)
+        {
+            res = (ccnum % 10) * 2
+            counteven += res / 10
+            counteven += res % 10
+        }
+    }
+}
