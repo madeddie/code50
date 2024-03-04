e399c937ac4afde5385c18f6ce130628953b21df
Edwin Hermans <edwin@madtech.cx>
2024-03-04 21:46:02 +0000

    Mon, Mar 4, 2024, 4:46 PM -05:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,6 +5,16 @@ int main(void)
 {
     string text = "One fish. Two fish. Red fish. Blue fish.";
     // string text = get_string("Text: ");
-    int words = 0;
+    int letters = 0;
+    int spaces = 0;
+    int punctuation = 0;
 
+    for (i = 0; len = strlen(text); i < len; i++)
+    {
+        if (isalpha(text[i]))
+            letters++
+        else if (isblank(text[i]))
+            spaces++
+        else if (text[i] == '!' || text[i] == '?' || text[i] == '.')
+    }
 }
