3977def5c32ec47ff6ce4ba0897d46e1fca7d743
Edwin Hermans <edwin@madtech.cx>
2024-03-04 22:19:10 +0000

    Mon, Mar 4, 2024, 5:19 PM -05:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -7,20 +8,23 @@ int main(void)
 {
     string text = get_string("Text: ");
     int letters = 0;
-    int spaces = 0;
-    int punctuation = 0;
+    int words = 0;
+    int sentences = 0;
 
     for (int i = 0, len = strlen(text); i < len; i++)
     {
         if (isalpha(text[i]))
             letters++;
         else if (isblank(text[i]))
-            spaces++;
+            words++;
         else if (text[i] == '!' || text[i] == '?' || text[i] == '.')
-            punctuation++;
+            sentences++;
     }
-    float L = (letters / (spaces + 1)) * 100;
-    float S = (punctuation / (spaces + 1)) * 100;
+    // Add one word because the last one doesn't have a space after it.
+    words++;
+
+    float L = (float) letters / words * 100.0;
+    float S = (float) sentences / words * 100.0;
     float index = 0.0588 * L - 0.296 * S - 15.8;
-    printf("index: %f\n", index);
+    printf("index: %f\n", round(index));
 }
