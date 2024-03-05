c6d990ff986038bb169a018b947dcfabe68659f4
Edwin Hermans <edwin@madtech.cx>
2024-03-05 16:58:29 +0000

    Tue, Mar 5, 2024, 11:58 AM -05:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,7 +5,7 @@
 #include <string.h>
 
 void print_usage(string command);
-bool all_digits(string value);
+bool not_digits(string value);
 
 int main(int argc, string argv[])
 {
@@ -14,7 +14,7 @@ int main(int argc, string argv[])
         print_usage(argv[0]);
         return 1;
     }
-    else if (!all_digits(argv[1]))
+    else if (not_digits(argv[1]))
     {
         print_usage(argv[0]);
         return 1;
@@ -27,14 +27,14 @@ void print_usage(string command)
     printf("Usage: %s key\n", command);
 }
 
-bool all_digits(string value)
+bool not_digits(string value)
 {
     for (int i = 0, len = strlen(value); i < len; i++)
     {
         if (!isdigit(value[i]))
         {
-            return 0;
+            return 1;
         }
     }
-    return 1;
+    return 0;
 }
