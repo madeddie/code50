f4d0881fadee1ac3f8120892321be979092f8db0
Edwin Hermans <edwin@madtech.cx>
2024-03-05 16:27:06 +0000

    Tue, Mar 5, 2024, 11:27 AM -05:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,34 +3,18 @@
 #include <stdio.h>
 #include <string.h>
 
-void rotate(int key, string text);
-int main(void)
+void print_usage(string command);
+
+int main(int argc, string argv[])
 {
-    string text = get_string("Text ");
-    // printf("%s\n", rotate(13, text));
-    rotate(13, text);
+    if (argc != 2)
+    {
+        print_usage(argv[0]);
+        return 1;
+    }
 }
 
-void rotate(int key, string text)
+void print_usage(string argv[])
 {
-    char new_string[strlen(text)];
-    for (int i = 0, len = strlen(text); i < len; i++)
-    {
-        char new_char;
-        if (isalpha(text[i]))
-        {
-            new_char = text[i] - key;
-            if (new_char < 65)
-            {
-                new_char = 'Z' - (64 - new_char);
-            }
-        }
-        else
-        {
-            new_char = text[i];
-        }
-        new_string[i] = new_char;
-        printf("%c", new_char);
-    }
-    printf("\n");
+    printf("Usage: %s key\n")
 }
