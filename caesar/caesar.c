66dcb30a80d1fda6cd923cbe21533a9ef6104921
Edwin Hermans <edwin@madtech.cx>
2024-03-05 03:13:17 +0000

    Mon, Mar 4, 2024, 10:13 PM -05:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,9 +3,12 @@
 #include <stdio.h>
 #include <string.h>
 
+string rotate(int key, string text);
+
 int main(void)
 {
-
+    string text = get_string("Text ");
+    printf("%s\n", rotate(13, text));
 }
 
 string rotate(int key, string text)
@@ -13,9 +16,10 @@ string rotate(int key, string text)
     string new_string = "";
     for (int i = 0, len = strlen(text); i < len; i++)
     {
+        char new_char;
         if (isalpha(text[i]))
         {
-            char new_char = text[i] - key;
+            new_char = text[i] - key;
             if (new_char < 65)
             {
                 new_char = 'Z' - (64 - new_char);
@@ -23,8 +27,9 @@ string rotate(int key, string text)
         }
         else
         {
-            char new_char = text[i];
+            new_char = text[i];
         }
         new_string += new_char;
     }
+    return new_string;
 }
