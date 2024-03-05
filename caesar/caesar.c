a4269539098b10c29757619d5f8199f74db21c91
Edwin Hermans <edwin@madtech.cx>
2024-03-05 02:00:46 +0000

    Mon, Mar 4, 2024, 9:00 PM -05:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-char ALPHA[] = {'a', 'b', 'c', 'd', 'e', 'f'};
-
 int main(void)
 {
-    printf("char 2 %c, char -5 %c\n", ALPHA[2], ALPHA[2 - 13]);
+
+}
+
+string rotate(int key, string text)
+{
+    for (i = 0, len = strlen(text); i < len; i++)
+    {
+        
+    }
 }
