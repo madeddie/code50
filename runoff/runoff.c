2a50a112e858aa94c64493f7e8d78ccf5f238ea5
Edwin Hermans <edwin@madtech.cx>
2024-03-07 17:48:33 +0000

    Thu, Mar 7, 2024, 12:48 PM -05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -145,9 +145,10 @@ void tabulate(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (candidates[preference[i][j]].eliminated == false)
+            if (candidates[preferences[i][j]].eliminated != false)
             {
-                
+                candidates[preferences[i][j]].votes += 1;
+                break;
             }
         }
     }
