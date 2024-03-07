59306e45cacf5bbb818f4c4508112230692ad9b8
Edwin Hermans <edwin@madtech.cx>
2024-03-07 21:41:03 +0000

    Thu, Mar 7, 2024, 4:41 PM -05:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -153,7 +153,7 @@ void sort_pairs(void)
     printf("before sorting\n");
     for (int i = 0; i < pair_count; i++)
     {
-        printf("%s, %s". candidates[pairs[i].winner], candidates[pairs[i].loser])
+        printf("%s, %s\n". candidates[pairs[i].winner], candidates[pairs[i].loser]);
     }
     for (int i = 0; i < pair_count - 1; i++)
     {
@@ -167,7 +167,7 @@ void sort_pairs(void)
     printf("after sorting\n");
     for (int i = 0; i < pair_count; i++)
     {
-        printf("%s, %s". candidates[pairs[i].winner], candidates[pairs[i].loser])
+        printf("%s, %s\n". candidates[pairs[i].winner], candidates[pairs[i].loser]);
     }
 }
 
