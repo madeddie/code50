9ae47fdcca1ba2d1e4b50dc71e2bef87acec2065
Edwin Hermans <edwin@madtech.cx>
2024-03-07 17:57:44 +0000

    Thu, Mar 7, 2024, 12:57 PM -05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -157,15 +157,34 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes >= voter_count / 2.0)
+        {
+            printf("%s\n", candidates[i].name);
+            return true;
+        }
+    }
     return false;
 }
 
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    return 0;
+    int min_vote = voter_count;
+    int min_vote_candidate = -1;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated == false)
+        {
+            if (candidates[i].votes < min_vote)
+            {
+                min_vote = candidates[i].votes;
+                min_vote_candidate = i;
+            }
+        }
+    }
+    candidates[min_vote_candidate].eliminated = true;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
