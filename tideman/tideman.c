0301ac8872c486171cf6f961b5ef151874105881
Edwin Hermans <edwin@madtech.cx>
2024-03-07 20:27:49 +0000

    Thu, Mar 7, 2024, 3:27 PM -05:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -122,8 +122,7 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
-    return;
+    for 
 }
 
 // Sort pairs in decreasing order by strength of victory
