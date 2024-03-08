0f3abe1abb7ddb21afec6b7cfb3eafbbee33f6ee
Edwin Hermans <edwin@madtech.cx>
2024-03-08 15:15:39 +0000

    Fri, Mar 8, 2024, 10:15 AM -05:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -117,7 +117,7 @@ void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count - 1; i++)
     {
-        for (int j = 1; j < candidate_count -1; j++)
+        for (int j = i + 1; j < candidate_count -1; j++)
         {
             preferences[ranks[i]][ranks[j]] += 1;
         }
