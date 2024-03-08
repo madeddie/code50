dceb670c34da772135f59bfe123f103b602e96e0
Edwin Hermans <edwin@madtech.cx>
2024-03-08 15:32:30 +0000

    Fri, Mar 8, 2024, 10:32 AM -05:00

--- volume/volume.c
+++ volume/volume.c
@@ -37,8 +37,19 @@ int main(int argc, char *argv[])
     uint8_t header[HEADER_SIZE];
     fread(header, HEADER_SIZE, 1, input);
     fwrite(header, HEADER_SIZE, 1, output);
-    
-    // TODO: Read samples from input file and write updated data to output file
+
+    // Create a buffer for a single sample
+    int16_t buffer;
+
+    // Read single sample from input into buffer while there are samples left to read
+    while (fread(&buffer, sizeof(int16_t), 1, input))
+    {
+        // Update volume of sample
+        buffer *= factor;
+
+        // Write updated sample to new file
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
 
     // Close files
     fclose(input);
