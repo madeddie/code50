3a73eb67fb2638a4e0dc2887d016fc14cdcc48c8
Edwin Hermans <edwin@madtech.cx>
2024-03-13 16:13:58 +0000

    Wed, Mar 13, 2024, 12:13 PM -04:00

--- test_twttr2/test_twttr.py
+++ test_twttr2/test_twttr.py
@@ -3,4 +3,8 @@ from twttr import shorten
 def test_lower():
     assert shorten("hello") == "hll"
 def test_capital():
-    assert shorten("HELLO") == "
+    assert shorten("HELLO") == "HLL"
+def test_num():
+    assert shorten("1hell") == "hll"
+def test_pun():
+    assert shorten("?HaLL") == "HLL"
