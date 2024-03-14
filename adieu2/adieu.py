c2368926ca25623324c1e8e41e87fe9181278a7e
Edwin Hermans <edwin@madtech.cx>
2024-03-14 18:55:11 +0000

    Thu, Mar 14, 2024, 2:55 PM -04:00

---
+++ adieu2/adieu.py
@@ -0,0 +1,13 @@
+import inflect
+
+p = inflect.engine()
+name_list = []
+while True:
+    try:
+        name = input("Name: ")
+        name_list.append(name)
+    except EOFError:
+        print()
+        break
+final_list = p.join(name_list)
+print("Adieu, adieu to " + final_list)
