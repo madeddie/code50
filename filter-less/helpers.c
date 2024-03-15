293f7ce843d058f57eb7b6c9e38d30fe88b7b550
Edwin Hermans <edwin@madtech.cx>
2024-03-15 14:05:42 +0000

    Fri, Mar 15, 2024, 10:05 AM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -41,7 +41,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j <= width / 2; j++)
+        for (int j = 0; j < width / 2; j++)
         {
             // printf("%i, %i\n", j, (width - 1) - j);
             RGBTRIPLE tmp = image[i][j];
