b651c563dd85d2a826dfcabfdd773cb2af5231be
Edwin Hermans <edwin@madtech.cx>
2024-03-15 21:16:15 +0000

    Fri, Mar 15, 2024, 5:16 PM -04:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -48,9 +48,13 @@ person *create_family(int generations)
         person *parent0 = create_family(generations - 1);
         person *parent1 = create_family(generations - 1);
 
-        // TODO: Set parent pointers for current person
+        // Set parent pointers for current person
+        new_person->parents[0] = parent0;
+        new_person->parents[1] = parent1;
 
-        // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        // Randomly assign current person's alleles based on the alleles of their parents
+        new_person->alleles[0] = parent0->alleles[rand() % 2];
+        new_person->alleles[1] = parent1->alleles[rand() % 2];
     }
 
     // If there are no generations left to create
