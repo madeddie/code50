bbb794eb35bab61879f037198e9b58e6f89ae0ac
Edwin Hermans <edwin@madtech.cx>
2024-03-15 15:15:16 +0000

    Fri, Mar 15, 2024, 11:15 AM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -9,7 +9,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int avg = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+            int avg =
+                round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
             image[i][j].rgbtBlue = avg;
             image[i][j].rgbtGreen = avg;
             image[i][j].rgbtRed = avg;
