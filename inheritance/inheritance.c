67a7170c026722e61fdd588db3af193c7003c924
Edwin Hermans <edwin@madtech.cx>
2024-03-15 21:19:23 +0000

    Fri, Mar 15, 2024, 5:19 PM -04:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -63,11 +63,14 @@ person *create_family(int generations)
         // Set parent pointers to NULL
         new_person->parents[0] = NULL;
         new_person->parents[1] = NULL;
-        // TODO: Randomly assign alleles
+
+        // Randomly assign alleles
+        new_person->alleles[0] = random_allele();
+        new_person->alleles[1] = random_allele();
     }
 
-    // TODO: Return newly created person
-    return NULL;
+    // Return newly created person
+    return new_person;
 }
 
 // Free `p` and all ancestors of `p`.
